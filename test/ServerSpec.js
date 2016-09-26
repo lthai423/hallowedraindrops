aa254fc8844260459ed2793e0224d896fa59f5bb
lthai423 <lthai423@gmail.com>
2016-09-26 14:58:15 -0700

    Implemented Grunt task management configuration file.

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -1,8 +1,8 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+var should = require('chai').should();
+describe('Server Side Specifications', function() {
+  describe('Test function', function() {
+    it('it should pass a test function', function() {
+      true.should.equal(true);
     });
   });
 });
