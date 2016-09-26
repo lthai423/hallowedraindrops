60ec6d2d587f6ebcecd2d1db7d9996d355d36b83
eli xian <elixian@gmail.com>
2016-09-26 10:37:15 -0700

    (fix) updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,20 @@ var BUILD_DIR = path.resolve(__dirname, 'client/public');
 var APP_DIR = path.resolve(__dirname, 'client/app');
 
 var config = {
-	entry: APP_DIR + '/index.jsx',
-	output: {
-		path: BUILD_DIR,
-		filename: 'bundle.js'
-	}
+  entry: APP_DIR + '/index.jsx',
+  output: {
+	  path: BUILD_DIR,
+	  filename: 'bundle.js'
+  },
+  module: {
+	  loaders: [
+		  {
+			test: /\.jsx?/,
+			include: APP_DIR,
+			loader: 'babel'
+		  }
+	  ]
+  }
 };
 
 module.exports = config;
