86f2d39b79ec3a4ef89410ee20f43b41b45f9316
Christopher Wong <chriskwong91@gmail.com>
2016-09-26 11:55:37 -0700

    Parses request body for post request to code

--- REPL_service/config/routes.js
+++ REPL_service/config/routes.js
@@ -4,10 +4,13 @@ module.exports = (app) => {
 
 app.route('/api/repl')
   .get((req, res) => {
-    var code = req.body;
+    var code = req.query;
+    console.log(code);
     res.send('Getting that REPL SERVER');
   })
   .post((req, res) => {
+    var code = req.body;
+    console.log(code);
     res.send('POSTING that REPL Server');
   });
 
