6a1a9bca59c2c29ec2714fc45651b47871a28702
Christopher Wong <chriskwong91@gmail.com>
2016-09-27 11:38:27 -0700

    Reformat route organization for services, add route to talk to client

---
+++ server/config/services.js
@@ -0,0 +1,4 @@
+// Services URL
+module.exports = {
+  REPL: 'http://localhost:3000/api/repl'
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 //This sets up the web server
 const express = require('express');
 const app = express();
+const services = require('./config/services.js');
+var replRouter = require('./resources/repl/replRoutes.js');
 
 //use middleware
 require('./config/middleware.js')(app, express);
@@ -8,8 +10,9 @@ require('./config/middleware.js')(app, express);
 // serve static files
 app.use(express.static(__dirname + '../client'));
 
-// routes
-require('./config/routes.js')(app);
+// use services
+app.use('/api/replservice', replRouter);
+// require('./config/routes.js')(app);
 
 // set port up
 var port = process.argv[2] || 8080;
