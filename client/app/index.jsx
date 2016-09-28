5c82d45c7223f4155264929626e5afaa4ca819cb
Christopher Wong <chriskwong91@gmail.com>
2016-09-28 16:08:44 -0700

    Put export default to bottom of each file

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Editor from './editor';
+import Navigation from './navigation';
+import output from './output';
 
 var Promise = require('bluebird');
 
