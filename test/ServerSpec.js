7c6efb14dbf8821f60ead3dc9e91db850a012443
Christopher Wong <chriskwong91@gmail.com>
2016-09-27 22:33:48 -0700

    Runs server when testing

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -1,9 +1,26 @@
 var should = require('chai').should();
 var expect = require('chai').expect;
-// var app = require('../server/server.js');
+var http = require('http');
+var webServer = require('../server/server.js');
+var replServer = require('../REPL_service/REPL_server.js');
 var request = require('request');
 
 describe('Server Side Specifications', function() {
+  var webInstance, replInstance;
+  before(function(done) {
+    webInstance = http.Server(webServer.app);
+    webInstance.listen(8080, () => {
+      var replInstance = http.Server(replServer.app);
+      replInstance.listen(3000, () =>{
+      done();
+      });
+    });
+  });
+  after(function() {
+    // console.log('closing servers');
+    if (webInstance) webInstance.close();
+    if (replInstance) replInstance.close();
+  });
   describe('Web Server', function() {
     it('should respond to POST request for /api/replservice/runcode with a 200 status code', function(done) {
       var requestParams = {
@@ -36,7 +53,7 @@ describe('Server Side Specifications', function() {
     });
 
     it('Should 404 when asked for a nonexistent endpoint', function(done) {
-      request('http://127.0.0.1:8080/pizza', function(error, response, body) {
+      request('http://localhost:8080/pizza', function(error, response, body) {
         expect(response.statusCode).to.equal(404);
         done();
       });
@@ -44,6 +61,12 @@ describe('Server Side Specifications', function() {
   });
 
   describe('REPL Service', function() {
+    before(function() {
+      // replServer.listen(3000);
+    });
+    after(function() {
+      // replServer.close();
+    });
     it('should respond to GET request for /api/repl with a 200 status code', function(done) {
       request('http://127.0.0.1:3000/api/repl', function(error, response, body) {
         expect(response.statusCode).to.equal(200);
