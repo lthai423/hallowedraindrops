4e54de688d1d1f0f6e426f87a3ccd0bd716709e6
Brian Zhou <bzhou4@gmail.com>
2016-09-28 11:29:08 -0700

    (test) added webpack-dev-server for front-end testing

---
+++ client/app/editor.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+var Promise = require('bluebird');
+
+export default class Editor extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+		  text: 'hello world', // text is going to be the code the user inputs
+		}
+	}
+
+	// componentDidMount() {
+	//   this.editor = this.editorSetup();
+	//   this.socket = this.setupSocket();
+	//   this.editor.setValue(this.state.text);
+	// }
+
+	render () {
+		return (
+		  <div>
+		    <div id="editor"></div>
+		    <button>Hello World </button>
+		  </div>
+		)
+	}
+}
+
+// module.exports = Editor; // 
