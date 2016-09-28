9a6fcec2587aa94571e80ef3f3fef8283b141319
Brian Zhou <bzhou4@gmail.com>
2016-09-28 14:55:05 -0700

    (add) made notes into the Gruntfile for when using grunt-mocha in the future

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
 
     mochaTest: {
       test: {
@@ -10,6 +12,20 @@ module.exports = function(grunt) {
         src: ['test/**/*.js']
       }
     },
+
+    // BELOW IS CODE FOR FRONT-END MOCHA TESTING WITH PHANTOMJS
+    // CURRENTLY IT DOESN'T WORK
+    // mocha: {
+    //   all: {
+    //     src: ['test/**/*.js'],
+    //   },
+    //   options: {
+    //     log: true,
+    //     logErrors: true,
+    //     growlOnSuccess: true,
+    //     run: true
+    //   }
+    // },
 
     nodemon: {
       dev: {
@@ -34,11 +50,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-nodemon');
-
+  grunt.loadNpmTasks('grunt-mocha');
 
   ////////////////////////////////////////////////////
   // Main grunt tasks
   ////////////////////////////////////////////////////
+
+  // grunt.registerTask('fet', [
+  //   'mocha'
+  // ]);
 
   grunt.registerTask('test', [
     'mochaTest'
