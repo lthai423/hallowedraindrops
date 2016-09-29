091f83c90d5e659e5fecc8d53124ae110964e965
lthai423 <lthai423@gmail.com>
2016-09-28 20:51:17 -0700

    Fixed 404.

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,21 +6,26 @@ var express = require('express');
 var Chance = require('chance'),
     chance = new Chance();
 
+var cached = {}, ukey = '';
+
 module.exports = (app, io) => {
   // app.use(express.static(__dirname + '/../../client/app'));
 
   app.route('/pad/create')
     .get((req, res) => {
-      var ukey = '/' + chance.string({length:5, pool:'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'});
-      console.log(ukey);
+      ukey = '/' + chance.string({length:5, pool:'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'});
       create_namespace(ukey, io);
+      cached[ukey] = ukey;
       res.send(ukey);
     });
 
   console.log(path.join(__dirname, '/../../client/pad.html'));
   app.route('/*')
     .get((req, res) => {
-      res.render('pad.html');
+      if (cached[req.path])
+        res.render('pad.html');
+      else
+        res.send(404);
     });
 
   // ** below code block is used for the server setup
