3673e6affec8f57eab73b0eb5eac595db9c81b97
Brian Zhou <bzhou4@gmail.com>
2016-09-29 21:47:15 -0700

    (fix) added in temp db

--- server/database/db.js
+++ server/database/db.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose');
 
 // in case machine can't find config.js, then have a config_example.js file for testing
 
-var db = require('../config/env/config.js').mongo;
+// var db = require('../config/env/config.js').mongo;
+// mongoose.connect(db.url); //connect to database
 
-mongoose.connect(db.url); //connect to database
+mongoose.connect('mongodb://localhost/codedrop');
+
 var Schema = mongoose.Schema;
 
 var githubSchema = new Schema({
