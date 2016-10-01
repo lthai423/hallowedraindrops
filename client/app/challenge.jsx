43eee68cf44a80960b815a35fa132e0c5c34af05
Brian Zhou <bzhou4@gmail.com>
2016-10-01 11:26:06 -0700

    (feat) added the challenge.jsx component

---
+++ client/app/challenge.jsx
@@ -0,0 +1,21 @@
+// component for a user to select that he/she wants to solve a problem
+
+import React from 'react';
+import { render } from 'react-dom';
+
+class Challenge extends React.Component {
+	// a sidebar that pops out listing all of the questions (TOP QUESTIONS)
+		// *label as only top questions, since we don't want to populate this field as
+		// *filled with questions since there should be other functionalities included
+			// *in the future, like real-time analyses on their code's complexities and such
+	// user selects one, and then the coderpad will pre-populate with that challenge
+		// if code is already on page, then a warning pops up first
+		constructor(props) {
+			super(props);
+			// state stuff if needed
+		}
+
+
+}
+
+module.exports = Challenge;
