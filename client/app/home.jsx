3c11dabfdb3616203495be6d1f9f272cc9ca7fd4
Brian Zhou <bzhou4@gmail.com>
2016-10-01 16:34:55 -0700

    (fix) cleared out index.html

--- client/app/home.jsx
+++ client/app/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Carousel from 'react-bootstrap/lib/carouselInstance';
 
 class Home extends React.Component {
 
@@ -7,4 +8,28 @@ class Home extends React.Component {
 		super(props);
 	}
 
+	githubAuth () {
+		document.location.href='/auth/github';
+	}
+
+	setPad() {
+		$.get('/pad/create', (data) {
+			window.location = data;
+		})
+	}
+
+
+	render() {
+		return (
+			<div>
+				<Carousel> Hello World</Carousel>
+				<button onClick={this.githubAuth.bind(this)}>Github</button>
+				<button onClick={this.setPad.bind(this)}>Pad</button>
+			</div>
+		)
+	}
 }
+
+export default Home;
+// have the page load in home.jsx
+// render(<App/>, document.getElementById('app'));
--- client/routes.js
+++ client/routes.js
@@ -7,13 +7,16 @@ import ReactDOM from 'react-dom';
 // other pages such as profile page
 import Index from './app/index.jsx';
 import Editor from './app/editor.jsx';
+import Home from './app/home.jsx';
 
 import { Router, Route, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
+// Changes in Routes.js include removing the '/' component to be 'App' to be 'Home'
+
 ReactDOM.render(
 	<Router history={hashHistory}>
-		<Route path='/' component={App}>
-			<IndexRoute component={home}></IndexRoute>
+		<Route path='/' component={Home} />
+			<IndexRoute component={Home}></IndexRoute>
 			<Route path='editor' component={Editor} />
 		</Route>
 	</Router>,
