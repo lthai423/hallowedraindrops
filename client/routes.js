ca6b71057aa8ba3d93811186de6205cc6cc4d626
Brian Zhou <bzhou4@gmail.com>
2016-10-01 17:19:42 -0700

    (fix) adjusted entry-point to enter through the router

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // other pages such as home-page
-// other pages such as login-page
 // other pages such as signup-page
 // other pages such as interviewer page
 // other pages such as profile page
@@ -9,16 +8,19 @@ import Index from './app/index.jsx';
 import Editor from './app/editor.jsx';
 import Home from './app/home.jsx';
 
+//*********** We need an App.jsx
+
 import { Router, Route, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
 // Changes in Routes.js include removing the '/' component to be 'App' to be 'Home'
 
+console.log('entered into the react-router');
+
 ReactDOM.render(
 	<Router history={hashHistory}>
-		<Route path='/' component={Home} />
+		<Route path="/" component={Home}>
 			<IndexRoute component={Home}></IndexRoute>
-			<Route path='editor' component={Editor} />
+			<Route path="/editor" component={Editor} />
 		</Route>
-	</Router>,
-		document.getElementById('app')
-);
+	</Router>
+	, document.getElementById('app'));
