c1ff91c129db6074b304fe1723dfacc62a45db91
Brian Zhou <bzhou4@gmail.com>
2016-10-01 19:13:21 -0700

    (fix) updated our home-page

--- client/app/home.jsx
+++ client/app/home.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+//react-bootstrap elements
 import Jumbotron from 'react-bootstrap/lib/Jumbotron.js';
+import Image from 'react-bootstrap/lib/Image.js';
+import Grid from 'react-bootstrap/lib/Grid.js';
+import Row from 'react-bootstrap/lib/Row.js';
+import Col from 'react-bootstrap/lib/Col.js';
 
 class Home extends React.Component {
 
@@ -25,8 +31,27 @@ class Home extends React.Component {
 				<Jumbotron className="banner"> 
 					<span className="banner-text">codedrop</span>
 				</Jumbotron>
-				<button onClick={this.githubAuth.bind(this)}>Github</button>
-				<button onClick={this.setPad.bind(this)}>Pad</button>
+
+				<div className="below-banner">
+					<span className="below-banner-text">Seting you for Success.</span>
+				</div>
+
+				<Grid>
+					<Row className="home-github">
+						<Col md={1} mdPush={5} className="vcenter">
+							<div className="home-selection">
+								<Image src="./styling/img/github-mark-resized-4.png" circle responsive onClick={this.githubAuth.bind(this)}/>
+							</div>
+						</Col>
+						<Col md={1} mdPush={5} className="vcenter">
+							<div className="home-selection">
+								<span className="home-terminal" onClick={this.setPad.bind(this)}>     >_</span>
+							</div>
+						</Col>
+					</Row>
+				</Grid>
+
+
 			</div>
 		)
 	}
