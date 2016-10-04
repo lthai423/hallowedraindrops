ddfe25903808de44638cbbc6c661f656584c758e
Christopher Wong <chriskwong91@gmail.com>
2016-10-04 10:30:18 -0700

    Option to create new db if it doesnt exist

--- server/database/config.js
+++ server/database/config.js
@@ -1,4 +1,5 @@
 var Sequelize = require('sequelize');
+var pg = require('pg');
 var sequelize = new Sequelize('codedrop', 'hallowedraindrops', 'codedrop', {
   host: 'localhost',
   dialect: 'postgres',
@@ -19,6 +20,31 @@ sequelize
     console.log('Unable to connect to the database:', err);
   });
 
-  module.exports = {
-    sequelize: sequelize,
-  };
+
+//possible use to create DB if it doesn't exist
+//NOT being used right now
+var pgInit = function(callback) {
+  var dbName = 'codedrop',
+      username = 'hallowedraindrops',
+      password = 'codedrop',
+      host = 'localhost';
+
+  var conStringPri = 'postgres://' + username + ':' + password + '@' + host + '/postgres';
+  var conStringPost = 'postgres://' + username + ':' + password + '@' + host + '/' + dbName;
+
+  // connect to postgres db
+  pg.connect(conStringPri, function(err, client, done) {
+      // create the db and ignore any errors, for example if it already exists.
+      client.query('CREATE DATABASE ' + dbName, function(err) {
+          //db should exist now, initialize Sequelize
+          var sequelize = new Sequelize(conStringPost);
+          callback(sequelize);
+          client.end(); // close the connection
+      });
+  });
+};
+
+module.exports = {
+  init: pgInit,
+  sequelize: sequelize,
+};
