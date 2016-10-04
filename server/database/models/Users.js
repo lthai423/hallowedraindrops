3e08e44166809556d3708b5e2d40fd4789b9955e
Christopher Wong <chriskwong91@gmail.com>
2016-10-03 19:27:19 -0700

    Create User Schema with Sequelize

--- server/database/models/Users.js
+++ server/database/models/Users.js
@@ -1,5 +1,31 @@
-var bookshelf = require('./config');
+var sequelize = require('../config');
 
-var User = bookshelf.Model.extend({
 
+var User = sequelize.define('user', {
+  login: Sequelize.STRING,
+  id: Sequelize.STRING,
+  name: Sequelize.STRING,
+  avatar_url: Sequelize.STRING,
+  github_url: Sequelize.STRING,
+  repos_url: Sequelize.STRING,
+  email: Sequelize.STRING,
+  created_at: Sequelize.STRING,
+  updated_at: Sequelize.STRING,
+  company: Sequelize.STRING
 });
+
+sequelize.sync().then(function() {
+  return User.create({
+    username: 'janedoe',
+    birthday: new Date(1980, 6, 20)
+  });
+}).then(function(jane) {
+  console.log(jane.get({
+    plain: true
+  }));
+});
+
+
+module.exports = User;
+
+
