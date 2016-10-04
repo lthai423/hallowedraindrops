bcf8fe89db5cb52e35d11403c87fd98402b5e459
Brian Zhou <bzhou4@gmail.com>
2016-10-04 12:06:28 -0700

    updated backend routes to fit for front-end routing

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -35,7 +35,7 @@ module.exports = (app, express) => {
   app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
