40912e37f11452cdebf0b305b2c7e4d81a6a9279
Brian Zhou <bzhou4@gmail.com>
2016-10-05 17:32:38 -0700

    (update) the helperMapFns

--- Testing_service/config/helperMapFns/fns.js
+++ Testing_service/config/helperMapFns/fns.js
@@ -3,5 +3,29 @@
 
 module.exports = {
 
-		// return 'expects' that deal with functions
+/* RULES:
+ * subm variable rubric:
+ *	"fn" 		 -> a function 
+ *	"subm"	 -> the subm value for the function (?)
+ *	"ans" -> the ans value for the function (?)
+ *	"args"	 -> the arguments object
+ *	"num"		 -> a number we are testing against
+ */
+
+	// fn should exist
+	fnExist : function(fn, ans) {
+		should.exist(fn);
+	},
+
+	// fn should be a function
+	fnIsAFunction : function(fn, ans) {
+		fn.should.be.a.Function;
+	},
+
+	// fn should have X number of arguments
+	fnNumArgs : function(fn, num) {
+		fn.length.should.equal(num);
+	},
+
+
 }
--- Testing_service/config/helperMapFns/general.js
+++ Testing_service/config/helperMapFns/general.js
@@ -3,11 +3,54 @@
 
 
 module.exports = {
-    // a map for all general-related tests
-  equal : function(input, output) { 
-      expect(input).to.equal(output);
-    },
-    notEqual : function(input, output) {
-      expect(input).to.not.equal(output);
-  	}
+
+	/* RULES:
+	 * subm variable rubric:
+	 *	"fn" 		 -> a function  (N/A FOR THESE TESTS)
+	 *	"subm"	 -> the subm value for the function (?)
+	 *	"ans" -> the ans value for the function (?)
+	 *	"args"	 -> the arguments object
+	 *	"num"		 -> a number we are testing against
+	 *  "specificClass"  -> a class type
+	 */
+
+  // if ans deeply equals subm
+	equal : function(subm, ans) {
+    expect(subm).to.equal(ans); // strict equality
+  },
+
+  // if ans does not deeply equal ans
+  notEqual : function(subm, ans) {
+    expect(subm).to.not.equal(ans);
+	},
+
+	// if ans is not NaN
+	notNan : function(subm, ans) {
+		expect(subm).to.not.equal(NaN);
+		expect(ans).to.not.equal(NaN);
+	},
+
+	// if ans is truthy
+	isTruthy : function(subm, ans) {
+		expect(subm).to.be.ok();
+		expect(ans).to.be.ok();
+	},
+
+	// if ans is falsey
+	isFalsey : function(subm, ans) {
+		expect(subm).to.not.be.ok();
+		expect(ans).to.not.be.ok();
+	},
+
+	// if ans is of a Specific Class
+	isClassy : function(subm, ans, specificClass) {
+		expect(subm).to.be.a(specificClass);
+		expect(ans).to.be.a(specificClass);
+	}
+
+	// if ans is not of a Specific Class
+	isNotClassy: function(subm, ans, specificClass) {
+		expect(subm).to.not.be.a(specificClass);
+		expect(ans).to.not.be.a(specificClass);
+	}
 }
