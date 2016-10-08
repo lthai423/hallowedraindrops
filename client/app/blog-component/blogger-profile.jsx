f11c9ac021434465a0e584be72a67e6e3052824c
Brian Zhou <bzhou4@gmail.com>
2016-10-08 06:32:15 -0700

    (add) made adjustments to the blogger pages

--- client/app/blog-component/blogger-profile.jsx
+++ client/app/blog-component/blogger-profile.jsx
@@ -8,9 +8,13 @@ import Jumbotron from 'react-bootstrap/lib/Jumbotron.js';
 class BloggerProfile extends React.Component {
 	constructor(props) {
 		super(props)
-		// this.state = {
-		// 	//
-		// }
+		this.state = {
+			// backgroundUrl: 
+		}
+	}
+
+	componentDidMount () {
+
 	}
 
 	// need to make a pull from github or something
@@ -18,15 +22,25 @@ class BloggerProfile extends React.Component {
 	// image comes in as avatar_url: https://avatars.githubusercontent.com/u/5092263?v=3
 	// url: 
 
+	// need to find a better way to display their image....
+	// <Image src="https://www.hackreactorconnect.com/assets/photos/1469481559380.jpg" circle responsive width={64} height={64}/>
+// width={64} height={64}
+
+	/* the div with class 'blogger-banner-img' will be space where they can enter a photo */
 	render () {
 		return (
 			<div>
 				<Jumbotron>
-					<Image src="https://www.hackreactorconnect.com/assets/photos/1469481559380.jpg" circle responsive width={64} height={64}/>
+					<div className="blogger-banner-img">
+
+					</div>
+					<div className="banner-text-blog">
+						{this.props.name}
+					</div>
 					<div>
-						Github:   www.github.com/brianzhou13
+						<strong>Github:</strong>   www.github.com/brianzhou13
 						<br/>
-						LinkedIn: www.linkedin.com 
+						<strong>LinkedIn:</strong> www.linkedin.com 
 					</div>
 				</Jumbotron>
 			</div>
