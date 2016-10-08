bdcff34327b4ab87ec26174a0d5dd235ff7cbcd6
Brian Zhou <bzhou4@gmail.com>
2016-10-07 19:41:45 -0700

    (add) added middleware and route file

--- ANALYTICS_service/ANALYTICS_server.js
+++ ANALYTICS_service/ANALYTICS_server.js
@@ -3,13 +3,15 @@ const express = require('express');
 const app = express();
 
 // add in middleware here
+require('./config/middleware.js')(app, express);
 
 // add in routes as well
+require('./config/routes.js')(app);
 
 var port = process.argv[2] || 1337; 
 if(!module.parent) {
 	app.listen(port, () => {
-		console.log('Web Server listening on port' + port + '!');
+		console.log('Analytics Server listening on port' + port + '!');
 	});
 }
 
