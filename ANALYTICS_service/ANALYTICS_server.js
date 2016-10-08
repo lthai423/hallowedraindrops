7a7614e655c32b6f1d3b5b90abe6a3960174273f
Brian Zhou <bzhou4@gmail.com>
2016-10-07 19:22:32 -0700

    (add) a server for analytics

--- ANALYTICS_service/ANALYTICS_server.js
+++ ANALYTICS_service/ANALYTICS_server.js
@@ -1,1 +1,18 @@
-var
+//This sets up the web server
+const express = require('express');
+const app = express();
+
+// add in middleware here
+
+// add in routes as well
+
+var port = process.argv[2] || 1337; 
+if(!module.parent) {
+	app.listen(port, () => {
+		console.log('Web Server listening on port' + port + '!');
+	});
+}
+
+module.exports = {
+	app: app
+};
