ea820c407274ce481dee48188f6549b3ba7059d2
Brian Zhou <bzhou4@gmail.com>
2016-10-12 15:18:40 -0700

    (add) a new .jsx for howitworks page

---
+++ client/app/howItWorks.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+// 	<div className="banner-bottom" id="home-bottom">
+	// 			<div className="row">
+	// 				<div className = "home-desc col-md-6.push-md-3.pull-md-3"> We understand preparing for  a bootcamp can be difficult. Resarch is often the first part of the Bootcamp process, but it's often the most tedious. Whether it be finding materials to study or seeking relevant coding problems to solve, accurate and helpful information are a hassle to locate.
+	// 				</div>
+	// 			</div>
+	// 			<br/>
+	// 			<br/>
+	// 			<div className="row">
+	// 				<div className ="home-desc col-md-6.push-md-3.pull-md-3">Codedrop helps fill this content gap. We are working with many in industry to devise the most up-to-date and relevant problems that'll help you guage where you are in your studying process. Questions like, "Am I up to par in regards to Algorithms? How much more do I need to know about Closures?" can be easily answered by seeing how you fare against the many problems and tests we've set up. 
+	// 				</div>
+	// 			</div>
+	// 			<br/>
+	// 			<br/>
+	// 			<div className="row">
+	// 				<div className ="home-desc col-md-6.push-md-3.pull-md-3">But let's say you need help, or are simply looking for someone else to work through a problem with. And all of your friends are at work, and nobody you know is also studying for Bootcamp. No need to fret. Codedrop's Pair Programming feature allows you to queue up in our Partners Lobby to find others who are ready and willing to work alongside you. 
+	// 				</div>
+	// 			</div>
+	// 	</div>
+	// </div>
+
+	/*<div>
+							<Link to="/blog">Blog (temporary)</Link>
+						</div>*/
+
+class HowItWorks extends React.Component {
+
+	render() {
+		return (
+			<div> Hello World </div>
+
+		)
+	}
+}
