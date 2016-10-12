cec32a4e8f63fe030b12761c40c76b5ced40d166
Brian Zhou <bzhou4@gmail.com>
2016-10-12 15:20:16 -0700

    (fix) updated link on home-page to redirect to howitworks page

--- client/app/home.jsx
+++ client/app/home.jsx
@@ -42,36 +42,6 @@ class Home extends React.Component {
 
 	// whatever link they added in, throw in a post request with the id
 
-	// *may need a promise
-	// 1. user wants to create a pad
-	// 2. we give client back a unique pad_ID key
-	// 3. with that unique pad_ID key, we move them to that page
-	// 4. on our server side, we catch that route and then send them the pad.html using res.render
-	// 5. then for that specific route, we will serve
-
-	// 	<div className="banner-bottom" id="home-bottom">
-	// 			<div className="row">
-	// 				<div className = "home-desc col-md-6.push-md-3.pull-md-3"> We understand preparing for  a bootcamp can be difficult. Resarch is often the first part of the Bootcamp process, but it's often the most tedious. Whether it be finding materials to study or seeking relevant coding problems to solve, accurate and helpful information are a hassle to locate.
-	// 				</div>
-	// 			</div>
-	// 			<br/>
-	// 			<br/>
-	// 			<div className="row">
-	// 				<div className ="home-desc col-md-6.push-md-3.pull-md-3">Codedrop helps fill this content gap. We are working with many in industry to devise the most up-to-date and relevant problems that'll help you guage where you are in your studying process. Questions like, "Am I up to par in regards to Algorithms? How much more do I need to know about Closures?" can be easily answered by seeing how you fare against the many problems and tests we've set up. 
-	// 				</div>
-	// 			</div>
-	// 			<br/>
-	// 			<br/>
-	// 			<div className="row">
-	// 				<div className ="home-desc col-md-6.push-md-3.pull-md-3">But let's say you need help, or are simply looking for someone else to work through a problem with. And all of your friends are at work, and nobody you know is also studying for Bootcamp. No need to fret. Codedrop's Pair Programming feature allows you to queue up in our Partners Lobby to find others who are ready and willing to work alongside you. 
-	// 				</div>
-	// 			</div>
-	// 	</div>
-	// </div>
-
-/*<div>
-						<Link to="/blog">Blog (temporary)</Link>
-					</div>*/
 
 	render() {
 		return (
@@ -104,7 +74,7 @@ class Home extends React.Component {
 							</Col>
 						</Row>
 					</Grid>
-					<a className="a-home" href="#"><div className="home-works">How does it work?</div></a>
+					<a className="a-home" href="/howitworks"><div className="home-works">How does it work?</div></a>
 				</div>
 			</div>
 		)
