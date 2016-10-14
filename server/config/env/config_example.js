8e7c66fb100d9bdca005b818a916713dc5de7713
lthai423 <lthai423@gmail.com>
2016-10-13 17:12:52 -0700

    Fixed config_example.js for Continuous Integration

---
+++ server/config/env/config_example.js
@@ -0,0 +1,17 @@
+module.exports = {
+  github: {
+    GITHUB_CLIENT_ID : "",
+    GITHUB_CLIENT_SECRET : "",
+    GITHUB_CALLBACK_URL : ""
+  },
+
+  mongo: {
+    url: ''
+  },
+    postgres: {
+    user: '',
+    password: '',
+    db: '',
+    host:''
+  }
+}
